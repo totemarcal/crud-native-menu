81ec51f6cd7221d6f70b1b92fc141b3ac69e8d8c
totemarcal <totemarcal@gmail.com>
2021-04-09 20:53:26 -0300

    menu_navigation

---
+++ components/Cliente/ListCliente.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function ListCliente({}) {
+  return(
+    <View>
+        <Text>Lista Cliente</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export default ListCliente
+  
