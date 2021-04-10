2d80c80040234d943adb69c763d46e297ee21948
Jonathan Aflitos <jonathan.aflitos@gmail.com>
2021-04-09 21:52:05 -0300

    Criada tela de produto

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import ListCliente from './components/Cliente/ListCliente'
 import HomeScreen from './components/HomeScreen'
+import ListProduto from './components/Produtos/ListProduto'
 
 const options = {
     headerLeft: () => (
@@ -16,21 +17,14 @@ const options = {
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-<<<<<<< HEAD
-  return (
-    <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
-    </View>
-=======
   return (  
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen options={options} name="Home" component={HomeScreen} />
         <Drawer.Screen options={options} name="Cliente" component={ListCliente} />
+        <Drawer.Screen options={options} name="Produto" component={ListProduto} />
       </Drawer.Navigator>
     </NavigationContainer>
     
->>>>>>> 81ec51f6cd7221d6f70b1b92fc141b3ac69e8d8c
   );
 }
